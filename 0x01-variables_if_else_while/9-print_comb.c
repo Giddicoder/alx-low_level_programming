55377dd5f042ddf30eb143f81f90b9b5d2237867
Giddicoder <pharmgideongiddy@gmail.com>
2023-07-14 19:10:13 -0700

    Comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int e;
+
+	for (e = 0 ; e <= 9 ; e++)
+	{
+		putchar(e + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
