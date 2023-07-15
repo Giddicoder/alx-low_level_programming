98364c4e118e3c029af43c8b8966c75614583267
Giddicoder <pharmgideongiddy@gmail.com>
2023-07-14 17:44:20 -0700

    No ten

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int g;
+
+	for (g = 0 ; g <= 9 ; g++)
+		printf("%d", g);
+	printf("\n");
+	return (0);
+}
