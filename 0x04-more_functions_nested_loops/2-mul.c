9bb9ecda10b1607e3269285d435ca32d94e9147e
Giddicoder <pharmgideongiddy@gmail.com>
2023-07-20 15:49:43 -0700

    Multiplyyy

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - funtion that multiplies two parameters
+ * @a: 1st parameter
+ * @b: 2nd parameter
+ * Return: a by b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
